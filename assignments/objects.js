560f7dece4c41cc8da4b3ffc5ea4aa72836e0fd0
justiny2c <justiny2c@gmail.com>
2019-05-06 14:22:20 -0700

    WIP: Finished Objects StretchGoals

--- assignments/objects.js
+++ assignments/objects.js
@@ -95,17 +95,27 @@ console.log(antonietta.multiplyNums(3,4))
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
-
-const parent = {}
-
 // Log the parent object's name
 
-// Log the child's age
 
-// Log the name and age of the grandchild
+parent.speak = function (){
+  return "Hi, I'm the eldest here."
+};
 
-// Have the parent speak
+console.log(parent.speak())
 
 // Have the child speak
 
+parent.child.speak = function (){
+  return "Hi, I'm in the middle."
+};
+
+console.log(parent.child.speak())
+
 // Have the grandchild speak
+
+parent.child.grandchild.speak = function (){
+  return "Hi, I'm the youngest."
+};
+
+console.log(parent.child.grandchild.speak());
